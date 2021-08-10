c2f6fca4eec96834d60abe6acda46317451dc1ad
Hyundo <48778241+arawatki97@users.noreply.github.com>
2021-08-10 23:47:32 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
+from setuptools import setup
 
+setup(
+  name='hdstreetview',
+  version='0.1',
+  description='Get pictures and timelines of panoramas from Naver Map.'
+  url='https://github.com/arawatki97/streetview.git',
+  author='Hyundo_Kang',
+  author_email='arawatki97@gmail.com',
+  license='Hyundo_Kang',
+  packages=['hdstreetview'],
+  zip_safe=False,
+  install_requires=[
+    'requests',
+    'pandas',
+    'PIL',
+    'shutil'
+    'itertools'
+  ]
+)
