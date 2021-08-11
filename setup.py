0d0f6abe1fbf0956deb5e1317e8f0f832e15c9ce
Hyundo <48778241+arawatki97@users.noreply.github.com>
2021-08-11 10:28:35 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name='hdstreetview',
   version='0.1',
   description='Get pictures and timelines of panoramas from Naver Map.'
-  url='https://github.com/arawatki97/streetview.git',
+  url='https://github.com/arawatki97/hdstreetview.git',
   author='Hyundo_Kang',
   author_email='arawatki97@gmail.com',
   license='Hyundo_Kang',
