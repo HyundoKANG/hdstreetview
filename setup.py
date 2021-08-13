68a5e05126c861fce7ca835825ab4e8cf4c15cf1
Hyundo <48778241+arawatki97@users.noreply.github.com>
2021-08-13 12:18:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 with open('requirements.txt') as f:
 	requirements = f.read().splitlines()
 
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+	
+	
 def readme():
     with open('readme.md') as f:
         return f.read()
@@ -11,11 +15,11 @@ setup(
     name='hdstreetview',
     version='0.1',
     description='Get pictures and timelines of panoramas from Naver Map.',
-    long_description="hi",
+    long_description=long_description,
     url='https://github.com/arawatki97/hdstreetview',
     author='Hyundo_Kang',
     author_email='arawatki97@gmail.com',
-    license='MIT',
+    license='Hyundo_Kang',
     packages=['hdstreetview'],
     zip_safe=False,
     install_requires=requirements,
