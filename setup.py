23b31aca5b7d43631714689a94c277d8f7ce3503
Hyundo <48778241+arawatki97@users.noreply.github.com>
2021-08-13 12:35:38 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+# -*- encoding: cp949 -*-
+
 from setuptools import setup
 
 with open('requirements.txt') as f:
 	requirements = f.read().splitlines()
 
-with open("README.md", "r") as fh:
+with open("README.md", "r" encoding="utf-8") as fh:
 	long_description = fh.read()
 
 setup(
